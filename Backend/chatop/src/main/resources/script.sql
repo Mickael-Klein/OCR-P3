3c2fd6649b6f998ba5a9dea6b3147c3703cc68b5
Mika <mickaelklein@hotmail.fr>
2023-12-14 19:00:44 +0100

    Sql script updated

--- Backend/chatop/src/main/resources/script.sql
+++ Backend/chatop/src/main/resources/script.sql
@@ -1,32 +1,32 @@
 CREATE TABLE IF NOT EXISTS `USERS` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `email` varchar(255) UNIQUE,
-  `name` varchar(255),
-  `password` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `RENTALS` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `surface` numeric,
-  `price` numeric,
-  `picture` varchar(255),
-  `description` varchar(2000),
-  `owner_id` integer NOT NULL,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`)
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surface` int NOT NULL,
+  `price` decimal(10, 2) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `owner_id` int NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `MESSAGES` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `rental_id` integer,
-  `user_id` integer,
-  `message` varchar(2000),
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`)
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `rental_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `message` varchar(2000) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`) ON DELETE CASCADE
 );
